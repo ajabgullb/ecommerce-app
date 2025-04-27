945a70f66c15c57dbbb6c7542711cc231c1b5663
ajabgullb <ajabgullb.superior@gmail.com>
2025-04-27 09:58:22 -0700

    Make the Client Side Login and Signup Forms

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import { useForm, type FieldValues } from "react-hook-form"
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: {errors, isSubmitting},
+    reset
+  } = useForm()
+
+  const onSubmit = async (data: FieldValues) => {
+    // TODO: Submit to server
+    // ...
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    reset()
+  }
+
   return (
-    <div>LoginForm</div>
+    <form onSubmit={handleSubmit(onSubmit)}
+      className="w-60 h-3/4 mx-auto p-5 border rounded-md flex justify-center items-center flex-col gap-5"
+    >
+      <div>
+        <label htmlFor="email">
+          <Input
+            {...register("email", {
+              required: "Email is required",
+              minLength: {
+                value: 20,
+                message: "Email must be at least 20 characters"
+              },
+            })}
+            className="bg-gray-200 w-full"
+            placeholder="Email..."
+            type="email"
+          />
+          {errors.email && (
+            <p className="text-red-500">{`${errors.email.message}`}</p>
+          )}
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          <Input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters"
+            },
+          })}
+            className="bg-gray-200 w-full"
+            placeholder="Password..."
+            type="password"
+          />
+          {errors.password && (
+            <p className="text-red-500">{`${errors.password.message}`}</p>
+          )}
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">  
+          <Input
+          {...register("confirmPassword", {
+            required: "Confirm Password is required",
+            validate: (value) => value === getValues("password") || "Password must match",
+          })}
+            className="bg-gray-200 w-full"
+            placeholder="Confirm Password..."
+            type="password"
+          />
+          {errors.confirmPassword && (
+            <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+          )}
+        </label>
+      </div>
+
+      <div 
+      className="text-sm text-gray-500 flex gap-1"
+        > <span>Create an account</span> 
+        <Link to={"/signup"} className="underline">Signup</Link>
+      </div>
+
+      <Button
+        className="w-full cursor-pointer disabled:bg-gray-500"
+        disabled={isSubmitting}
+        type="submit"
+      >Submit</Button>
+    </form>
   )
 }
 
--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,8 +1,127 @@
-import React from 'react'
+import { useForm, type FieldValues } from "react-hook-form"
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Link } from "react-router-dom";
 
-export default function SignupForm() {
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: {errors, isSubmitting},
+    reset
+  } = useForm()
+
+  const onSubmit = async (data: FieldValues) => {
+    // TODO: Submit to server
+    // ...
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    reset()
+  }
+
   return (
-    <div>SignupForm</div>
+    <form onSubmit={handleSubmit(onSubmit)}
+      className="w-60 h-3/4 mx-auto p-5 border rounded-md flex justify-center items-center flex-col gap-5"
+    >
+      <div className="flex gap-2">
+        <label htmlFor="firstName">
+          <input
+            {...register("firstName", {
+              required: "First Name is required",
+            })}
+            className="bg-gray-200 w-full p-2 h-9 border rounded-lg"
+            placeholder="First"
+            type="text" 
+          />
+          {errors.firstName && (
+            <p className="text-red-500">{`${errors.firstName.message}`}</p>
+          )}
+        </label>
+        <label htmlFor="secondName">
+          <input
+            {...register("secondName", {
+              required: "Second Name is required",
+            })}
+            className="bg-gray-200 w-full p-2 h-9 border rounded-lg"
+            placeholder="Second"
+            type="text" 
+          />
+          {errors.secondName && (
+            <p className="text-red-500">{`${errors.secondName.message}`}</p>
+          )}
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="email">
+          <Input
+            {...register("email", {
+              required: "Email is required",
+              minLength: {
+                value: 20,
+                message: "Email must be at least 20 characters"
+              },
+            })}
+            className="bg-gray-200 w-full"
+            placeholder="Email..."
+            type="email"
+          />
+          {errors.email && (
+            <p className="text-red-500">{`${errors.email.message}`}</p>
+          )}
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="password">
+          <Input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters"
+            },
+          })}
+            className="bg-gray-200 w-full"
+            placeholder="Password..."
+            type="password"
+          />
+          {errors.password && (
+            <p className="text-red-500">{`${errors.password.message}`}</p>
+          )}
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="password">  
+          <Input
+          {...register("confirmPassword", {
+            required: "Confirm Password is required",
+            validate: (value) => value === getValues("password") || "Password must match",
+          })}
+            className="bg-gray-200 w-full"
+            placeholder="Confirm Password..."
+            type="password"
+          />
+          {errors.confirmPassword && (
+            <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+          )}
+        </label>
+      </div>
+
+      <div 
+      className="text-sm text-gray-500 flex gap-1"
+        > <span>Already have an account</span> 
+        <Link to={"/login"} className="underline">Login</Link>
+      </div>
+
+      <Button
+        className="w-full cursor-pointer disabled:bg-gray-500"
+        disabled={isSubmitting}
+        type="submit"
+      >Submit</Button>
+    </form>
   )
 }
 
